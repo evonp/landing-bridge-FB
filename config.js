00835ab52fdfb4b4c3e2bde6248c4dbc9d0e0de7
evonp <ttnf91801@gmail.com>
2025-06-13 13:17:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const REDIRECT_BASE_URL = "https://media.xtjqymmo.com/pwa_land_page/redirect2?type=4";
-const REDIRECT_DELAY = 500;
+// 配置文件
+const REDIRECT_BASE_URL = "https://media.xtjqymmo.com/pwa_land_page/redirect2?type=4&wpsrc=Facebook";
 const FB_PIXEL_ID = "1051298070546083";
-
+const REDIRECT_DELAY = 500;
